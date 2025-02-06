612b5928b343973d183f0334e08a9edb3db79840
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-02-06 18:01:15 +0530

    a^

---
+++ 1. JAN 23/P4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function to compute a^b using Divide and Conquer (Exponentiation by Squaring)
+long long power(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long half = power(a, b / 2);
+    if (b % 2 == 0)
+        return half * half;
+    else
+        return a * half * half;
+}
+
+int main() {
+    long long a, b;
+    cout << "Enter base (a): ";
+    cin >> a;
+    cout << "Enter exponent (b): ";
+    cin >> b;
+
+    cout << a << "^" << b << " = " << power(a, b) << endl;
+    return 0;
+}
