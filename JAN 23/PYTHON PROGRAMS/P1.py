95cabe6a6979a7b5b15daa607d895961a60b8fea
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-04-14 01:13:10 +0530

    Modification in P1

--- JAN 23/PYTHON PROGRAMS/P1.py
+++ JAN 23/PYTHON PROGRAMS/P1.py
@@ -8,9 +8,8 @@ import matplotlib.pyplot as mp
 # Defining Array of size 'n'
 def array(size):
     a=[]
-    for i in range (1,size):
+    for i in range (1,size+1):
         a.append(random.randint(1,size))
-    a.append(size+100)
     return a
 
 # Checking 1D Peak
@@ -21,34 +20,39 @@ def Peak(List):
         Peaks.append(0)
     
     stime=time.perf_counter()
-    for i in range(Num):
-            if(i>0 and i<Num-1):
-                if(List[i]>List[i-1] and List[i]>List[i+1]):
-                    Peaks[i]=1
-            if(i==0):
-                if(List[i]>List[i+1]):
-                    Peaks[i]=1
-            if(i==Num-1):
-                if(List[i]>List[i-1]):
-                    Peaks[i]=1
+    if(len(List)>1):
+        for i in range(Num):
+                if(i>0 and i<Num-1):
+                    if(List[i]>List[i-1] and List[i]>List[i+1]):
+                        Peaks[i]=1
+                if(i==0):
+                    if(List[i]>List[i+1]):
+                        Peaks[i]=1
+                if(i==Num-1):
+                    if(List[i]>List[i-1]):
+                        Peaks[i]=1
+    elif(len(List)==1):
+        Peaks[0]=1
+    else:
+        Peaks=[]
     etime=time.perf_counter()
     Time=(etime-stime)
-    print("Peaks are at these positions:", Peaks)
-    return Time
+    PnT=[Peaks,Time]
+    '''
+    if(Num<=100):
+        print("Peaks are at these positions:", Peaks)
+    '''
+    return PnT
 
 # Basis of 1D Peak Program
 def User():
     Num=int(input("Enter number of Elements in List:\t"))
-    List=[]
-
-    for i in range(Num):
-        print("Enter element",i+1,end="")
-        temp=int(input(": "))
-        List.append(temp)
-    
-    Time=Peak(List)
+    List=array(Num)
+    Ans=Peak(List)
+    Peaks=Ans[0]
+    Time=Ans[1]
     # Printing Answer
-    print("\nTime Taken = ",Time)
+    print("List: ", List, "\nPeaks are: ", Peaks, "\nTime Taken = ", Time)
 
 # Time Complexity for "1D Peak"
 def TC():
@@ -56,11 +60,13 @@ def TC():
     Table1=tuple()
     T=[]
     N=[]
-    while(n<=10**6):
+    n=50
+    while(n<=10**4):
         Tdiff=0
         List=array(n)
         for i in range(100):
-            Time=Peak(List)
+            Ans=Peak(List)
+            Time=Ans[1]
             Tdiff+=Time
         Tdiff/=100
         T.append(Tdiff)
@@ -71,17 +77,20 @@ def TC():
     return Table1
 
 # User Input
-In = int(input("Enter 0 for Sample Program\tand\t1 for Time Complexity Graph for 1d Peak:\n"))
+In = int(input("\n1. For Sample Program: Enter 0\n2. For Time Complexity Graph: Enter 1\nOption "))
 if(In==0):
     User()
-else:
+elif(In==1):
     Table=TC()
 
-# Plotting Time Complexity Graph
-y=Table[0]
-x=Table[1]
-mp.title("Time Complexity for\nLinear Search using Iteration")
-mp.plot(x,y)
-mp.xlabel("Observations")
-mp.ylabel("Time")
-mp.show()
+    # Plotting Time Complexity Graph
+    y=Table[0]
+    x=Table[1]
+    print("Obs:", x, "\nTime:", y)
+    mp.title("Time Complexity for\n\'1-D Peak\'")
+    mp.plot(x,y)
+    mp.xlabel("Observations")
+    mp.ylabel("Time")
+    mp.show()
+else:
+    print("\nInvalid Input!")
