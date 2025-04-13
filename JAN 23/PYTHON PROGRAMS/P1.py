c8853746858c82cb615cd86a2ea28aabb448a228
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-04-14 00:25:29 +0530

    1D Peak

--- JAN 23/PYTHON PROGRAMS/P1.py
+++ JAN 23/PYTHON PROGRAMS/P1.py
@@ -5,10 +5,22 @@ import time
 import random
 import matplotlib.pyplot as mp
 
+# Defining Array of size 'n'
+def array(size):
+    a=[]
+    for i in range (1,size):
+        a.append(random.randint(1,size))
+    a.append(size+100)
+    return a
+
 # Checking 1D Peak
 def Peak(List):
     Num = len(List)
     Peaks = []
+    for i in range(Num):
+        Peaks.append(0)
+    
+    stime=time.perf_counter()
     for i in range(Num):
             if(i>0 and i<Num-1):
                 if(List[i]>List[i-1] and List[i]>List[i+1]):
@@ -19,6 +31,10 @@ def Peak(List):
             if(i==Num-1):
                 if(List[i]>List[i-1]):
                     Peaks[i]=1
+    etime=time.perf_counter()
+    Time=(etime-stime)
+    print("Peaks are at these positions:", Peaks)
+    return Time
 
 # Basis of 1D Peak Program
 def User():
@@ -29,20 +45,43 @@ def User():
         print("Enter element",i+1,end="")
         temp=int(input(": "))
         List.append(temp)
-    st=time.time()
-    Peaks=Peak(List)
-    et=time.time()
-    time=(et-st)
+    
+    Time=Peak(List)
     # Printing Answer
-    print("Peaks are at these positions:", Peaks, "\nTime Taken = ",time)
+    print("\nTime Taken = ",Time)
 
 # Time Complexity for "1D Peak"
 def TC():
-    ""
+    m=float(input("Enter Size multiplier:\t"))
+    Table1=tuple()
+    T=[]
+    N=[]
+    while(n<=10**6):
+        Tdiff=0
+        List=array(n)
+        for i in range(100):
+            Time=Peak(List)
+            Tdiff+=Time
+        Tdiff/=100
+        T.append(Tdiff)
+        N.append(n)
+        n*=m
+        n=int(n)
+    Table1+=(T,N)
+    return Table1
 
 # User Input
 In = int(input("Enter 0 for Sample Program\tand\t1 for Time Complexity Graph for 1d Peak:\n"))
 if(In==0):
     User()
 else:
-    ""
+    Table=TC()
+
+# Plotting Time Complexity Graph
+y=Table[0]
+x=Table[1]
+mp.title("Time Complexity for\nLinear Search using Iteration")
+mp.plot(x,y)
+mp.xlabel("Observations")
+mp.ylabel("Time")
+mp.show()
