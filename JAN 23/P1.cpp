d50a836296815928cbbdb9b0a9b0014f1c72a1bb
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-04-16 17:59:31 +0530

    1D Peak Program

--- JAN 23/P1.cpp
+++ JAN 23/P1.cpp
@@ -1,33 +1,64 @@
 // Program for 1D Peak
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter number of elements: ";
-    cin>>n;
+using namespace std::chrono;
 
-    // Creating Array of n elements and an array for finding Peaks
-    int Array[n],Peak[n];
-    for(int i=0; i<n; i++){
-        cin >> Array[i];
-        Peak[i]=0;
+// Creating Array of n elements
+void CreateArray(int a, int *A, int *P){
+    srand(time(0));
+    for(int i=0; i < a; i++){
+        A[i] = rand()%(a+100);
+        P[i] = 0;
     }
-
-    // Finding Peaks in given 1-Dimensional array
+}
+// Finding Peaks
+nanoseconds Peaks(int n, int *A, int *P){
+    auto start = high_resolution_clock::now();
     for(int i=0; i<n; i++){
         if(i>0 && i<n-1)
-            if(Array[i]>Array[i-1] && Array[i]>Array[i+1])
-                Peak[i]=1;
-
-        if((i==0) && (Array[i]>Array[i+1]))
-            Peak[i]=1;
-            
-        if(i==n-1)
-            if(Array[i]>Array[i-1])
-                Peak[i]=1;
+            if(*A>*(A+1) && *A>*(A-1))
+                *P=1;
+        else if((i==0) && (*A>*(A+1)))
+            *P=1;
+        if(i==n-1 && *A>*(A-1))
+                *P=1;
+        A++; P++;
     }
+    auto end = high_resolution_clock::now();
+    auto duration= duration_cast<nanoseconds>(end-start);
+    //cout << "Time Taken by code: " << duration.count() << " nanoseconds" << endl;
+    return duration;
+}
 
-    // Output
+int main(){
+    int n = 500;
+    float m;
+    cout << "Enter Multiplier: "; cin >> m;
+    if(m==0 || m==1){
+        m=1.5;
+    }
+    while(n<100000){
+        int *Array = new int[n],*Peak = new int[n];
+        CreateArray(n, Array, Peak);
+        nanoseconds Time = nanoseconds::zero();
+        for(int i=0; i < 100; i++){
+            Time += Peaks(n, &Array[0], &Peak[0]);
+        }
+        auto Average = Time/100;
+        double seconds = Average.count()/1e9; // Converting nanoseconds to seconds.
+        cout << "For n = "<<n<< ", Average Time = "<<seconds<< fixed << setprecision(9) <<" seconds"<<endl;
+        delete [] Array;
+        delete [] Peak;
+
+        n*=m;
+    }
+    
+    // Output in case of 1 Observation
+    /*
     cout << "\n" << "Peak Array is: " << "[";
     for(int i=0; i<n; i++){
         cout << Peak[i];
@@ -36,6 +67,6 @@ int main(){
         }
     }
     cout << "]";
-
+    */
     return 0;
 }
